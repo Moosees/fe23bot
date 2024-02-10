b2d16bc47cab4957f01cf7ff2b31b08a21b7ccc5
Moosees <linus.almgren@gmail.com>
2024-02-10 21:45:26 +0100

    add subfolders for events, refactor folder search function to new getAllFilesInFolder function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import 'dotenv/config.js';
-import { readdirSync } from 'fs';
-import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
-
-const srcPath = import.meta.url;
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import { getAllFilesInFolder } from './tools/index.js';
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -12,32 +10,28 @@ client.commands = new Collection();
 client.cooldowns = new Collection();
 
 // Loading command files
-const commandFolders = readdirSync(new URL('./commands', srcPath));
+const commandFiles = getAllFilesInFolder('commands');
 
-for (const folder of commandFolders) {
-	const folderPath = './commands/' + folder;
-	const commandFiles = readdirSync(new URL(folderPath, srcPath))
-		.filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	// Dynamic import of default export from file
+	const { default: command } = await import(file);
 
-	for (const file of commandFiles) {
-		const filePath = folderPath + '/' + file;
-		const { default: command } = await import(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		}
-		else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	}
+	else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
 
 // Loading event files
-const eventFiles = readdirSync(new URL('./events', srcPath))
-	.filter(file => file.endsWith('.js'));
+const eventFiles = getAllFilesInFolder('events');
 
 for (const file of eventFiles) {
-	const { default: event } = await import('./events/' + file);
+	const { default: event } = await import(file);
+
+	// Add all events listeners to bot
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
 	}
