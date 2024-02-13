f329914c417d1135fd992ed0b350f930f6628913
Moosees <linus.almgren@gmail.com>
2024-02-13 23:26:03 +0100

    refactor tools files

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,26 +1,3 @@
-import { readdirSync } from 'fs';
-import { setTimeout } from "node:timers/promises";
-
-export const wait = setTimeout;
+export { setTimeout as wait } from "node:timers/promises";
 export { sequelize } from './sequelize.js';
-
-export const getAllFilesInFolder = (folderName) => {
-	const foundFiles = [];
-
-	const srcPath = import.meta.url;
-	const folderPath = '../' + folderName;
-	const subFolders = readdirSync(new URL(folderPath, srcPath));
-
-	for (const folder of subFolders) {
-		const subFolderPath = folderPath + '/' + folder;
-		const files = readdirSync(new URL(subFolderPath, srcPath))
-			.filter(file => file.endsWith('.js'));
-
-		for (const file of files) {
-			const filePath = subFolderPath + '/' + file;
-			foundFiles.push(new URL(filePath, srcPath).href);
-		}
-	};
-
-	return foundFiles;
-};
+export { getAllFilesInFolder } from './file-utils.js';
