66727494af44513c35ecb14df7075183825a084e
Linus Almgren <linus.almgren@gmail.com>
2024-02-18 00:53:15 +0100

    add sequelize force sync example

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,9 @@
 import { KattCount } from './katt-count.js';
 
 export const syncModels = async () => {
-	KattCount.sync();
+	KattCount.sync(); // sync the table to be ready to use or initialize it if it's new
+	// KattCount.sync({ force: true }) // force the table to be reset when syncing even if it already exists (and needs to be changed)
+
 	console.log('Syncing models');
 	console.log(await KattCount.findAll());
 };
